f2794befe523f74abfe4b240e1901e60924961cb
WesleySAllen <WesleySAllen@users.noreply.github.com>
2016-11-03 14:28:19 -0600

    Add files via upload

---
+++ MultiDice/Pig/testpig.java
@@ -0,0 +1,13 @@
+//wes allen
+import javax.swing.JFrame;
+class testpig
+{
+	public static void main(String[] args) 
+	{
+		Pig p1 = new Pig();
+		p1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p1.setSize(650, 400);
+		p1.setVisible(true);
+		
+	}
+}
