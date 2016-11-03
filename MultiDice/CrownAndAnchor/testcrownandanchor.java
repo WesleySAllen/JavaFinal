ef0c54005781e30dfa9635785f476975dc8f3a32
WesleySAllen <WesleySAllen@users.noreply.github.com>
2016-11-03 14:18:26 -0600

    Add files via upload

---
+++ MultiDice/CrownAndAnchor/testcrownandanchor.java
@@ -0,0 +1,13 @@
+//wes allen
+import javax.swing.JFrame;
+class testcrownandanchor
+{
+	public static void main(String[] args) 
+	{
+		CrownAndAnchor ca1 = new CrownAndAnchor();
+		ca1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ca1.setSize(650, 400);
+		ca1.setVisible(true);
+		
+	}
+}
