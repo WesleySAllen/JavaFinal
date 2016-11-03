dd9f99d7725e7352199ce4f0c5f7f3c753c5bb86
WesleySAllen <WesleySAllen@users.noreply.github.com>
2016-11-03 14:27:16 -0600

    Add files via upload

---
+++ MultiDice/LCR/testlcr.java
@@ -0,0 +1,13 @@
+//wes allen
+import javax.swing.JFrame;
+class testlcr
+{
+	public static void main(String[] args) 
+	{
+		LCR lcr1 = new LCR();
+		lcr1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		lcr1.setSize(650, 650);
+		lcr1.setVisible(true);
+		
+	}
+}
